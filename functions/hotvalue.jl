cd05babd99237eee82c464f8f394614cf7caba56
Smart <francis.smart.ctr@ad.dot.gov>
2019-08-02 10:58:51 -0400

    Playing around with idea of hotvalue

--- functions/hotvalue.jl
+++ functions/hotvalue.jl
@@ -1,9 +1,25 @@
 cd("C:/Users/francis.smart.ctr/GitDir/AbstractLogic")
 
-i=1
+struct hotcombination(invec)
+
+  Size = *(length.(invec))
+end
+
 function hotvalue(i, col, invec)
     indexvalue = Integer(ceil(i / (invec[(col+1):length(invec)] .|> length |> prod)))
     indexvalue = (indexvalue - 1) % length(invec[col])
     invec[col][indexvalue+1]
     #invec[col][indexvalue]
 end
+
+
+myset = [1:2, 1:2, 1:2, 1:2]
+
+hotvalue(12, 1, myset)
+[hotvalue(i, j, myset) for i in 1:16, j in 1:4]
+
+hotvalue(1, 1, [1:3,1:2,1:2])
+
+hotvalue(1, 1, [1:3,1:2,1:2])
+
+invec = Dict(a=1:2, b=1:4,c=1:4)
