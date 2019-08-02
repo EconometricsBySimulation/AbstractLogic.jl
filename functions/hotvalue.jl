e79fefe1d3df7c238812807ada65dcf8c657dda7
Smart <francis.smart.ctr@ad.dot.gov>
2019-08-02 13:26:34 -0400

    Playing around with idea of Hotcomb - including tests

--- functions/hotvalue.jl
+++ functions/hotvalue.jl
@@ -1,17 +1,17 @@
 cd("C:/Users/francis.smart.ctr/GitDir/AbstractLogic")
 
-struct Hotcombination
+struct Hotcomb
   intuple
 end
 
-Base.:keys(x::Hotcombination) = keys(x.intuple)
-value(x::Hotcombination) = [i for i in x.intuple]
-Base.:size(x::Hotcombination) = (*(length.(value(x))...), length(x.intuple))
-ncol(x::Hotcombination) = size(x)[2]
-nrow(x::Hotcombination) = size(x)[1]
+Base.keys(x::Hotcomb) = keys(x.intuple)
+value(x::Hotcomb) = [i for i in x.intuple]
+Base.size(x::Hotcomb) = (*(length.(value(x))...), length(x.intuple))
+ncol(x::Hotcomb) = size(x)[2]
+nrow(x::Hotcomb) = size(x)[1]
 find(x::Tuple, y::Symbol) = x[[x...] .== y]
 
-function Base.getindex(x::Hotcombination, row::Integer, col::Integer)
+function Base.getindex(x::Hotcomb, row::Integer, col::Integer)
     # Divisor is calculating based on how many values remains
     # how many times to repeat the current value (effectively)
     divisor = (col+1 > size(x)[2] ? 1 : value(x)[(col+1):size(x)[2]] .|> length |> prod)
@@ -21,43 +21,31 @@ function Base.getindex(x::Hotcombination, row::Integer, col::Integer)
     #invec[col][indexvalue]
 end
 
-Base.getindex(x::Hotcombination, row::Integer, col::Symbol) = x[row, find(keys(x), col)[1]]
-
-
-x = Hotcombination((a=1:2, b=1:4,c='a':'c'))
-keys(x)
-value(x)
-size(x)
-x[3,2]
-[x[i,j] for i in 1:size(x)[1], j in 1:size(x)[2]] # Seems to be working
-x[3,:b]
-
-first(keys(x)[[keys(x)...] .== col])
-
-
-names = keys(invec)
-vector = [i for i in invec]
-Size = (*(length.(vector)...), length(vector))
-
-function hotvalue(i, col, invec)
-    indexvalue = Integer(ceil(i / (invec[(col+1):length(invec)] .|> length |> prod)))
-    indexvalue = (indexvalue - 1) % length(invec[col])
-    invec[col][indexvalue+1]
-    #invec[col][indexvalue]
+function Base.getindex(x::Hotcomb, I...)
+  index = [I...]
+  (index[1] == Colon()) && (index[2] != Colon()) && (return [x[i,index[2]] for i = 1:size(x)[1] ])
+  (index[1] != Colon()) && (index[2] == Colon()) && (return [x[index[1],i] for i = 1:size(x)[2] ])
+  (index[1] == Colon()) && (index[2] == Colon()) && (return collect(x))
 end
 
+function Base.getindex(x::Hotcomb, row::Integer, col::Symbol)
+  keymatch = findall(y -> y==col, [keys(x)...])
+  length(keymatch)==0 && throw("Symbol :$col not found")
+  x[row, keymatch...]
+end
 
-invec = [1:2, 1:2, 1:2, 1:2]
+Base.collect(x::Hotcomb) = [x[i,j] for i = 1:size(x)[1], j = 1:size(x)[2]]
+###############################################################################
+# Testing
 
-hotvalue(12, 2, invec)
-hotvalue(12, 4, invec)
+using Test
 
-[hotvalue(i, j, invec) for i in 1:16, j in 1:4]
+mycomb = Hotcomb((a=1:2, b=1:4,c='a':'c'))
+@test keys(mycomb) == (:a, :b, :c)
+@test value(mycomb) == [1:2, 1:4, 'a':'c']
+@test size(mycomb) == (24,3)
+@test collect(mycomb) == mycomb[:,:] # Seems to be working
+@test mycomb[13,2] == mycomb[13,:b]
+@test mycomb[5,:] == [1, 2, 'b']
 
-hotvalue(1, 1, [1:3,1:2,1:2])
-
-hotvalue(1, 1, [1:3,1:2,1:2])
-
-invec |> length
-
-keys(invec)
+@test_throws "Symbol :d not found" mycomb[3,:d]
