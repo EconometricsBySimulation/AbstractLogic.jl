c85189d5d47282213649d95a3ebdcb6abfeb65a7
Francis C Smart <Fsmart@gmail.com>
2019-08-30 10:03:44 -0400

    Project.toml updates

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,51 @@
-using Documenter
+
+using Documenter, AbstractLogic
+
+
+# using Documenter, DataFrames
+#
+# # Workaround for JuliaLang/julia/pull/28625
+# if Base.HOME_PROJECT[] !== nothing
+#     Base.HOME_PROJECT[] = abspath(Base.HOME_PROJECT[])
+# end
+#
+# # Build documentation.
+# # ====================
+#
+# makedocs(
+#     # options
+#     modules = [DataFrames],
+#     doctest = false,
+#     clean = false,
+#     sitename = "DataFrames.jl",
+#     format = Documenter.HTML(),
+#     pages = Any[
+#         "Introduction" => "index.md",
+#         "User Guide" => Any[
+#             "Getting Started" => "man/getting_started.md",
+#             "Joins" => "man/joins.md",
+#             "Split-apply-combine" => "man/split_apply_combine.md",
+#             "Reshaping" => "man/reshaping_and_pivoting.md",
+#             "Sorting" => "man/sorting.md",
+#             "Categorical Data" => "man/categorical.md",
+#             "Missing Data" => "man/missing.md",
+#             "Data manipulation frameworks" => "man/querying_frameworks.md"
+#         ],
+#         "API" => Any[
+#             "Types" => "lib/types.md",
+#             "Functions" => "lib/functions.md",
+#             "Indexing" => "lib/indexing.md"
+#         ]
+#     ]
+# )
+#
+# # Deploy built documentation from Travis.
+# # =======================================
+#
+# deploydocs(
+#     # options
+#     repo = "github.com/JuliaData/DataFrames.jl.git",
+#     target = "build",
+#     deps = nothing,
+#     make = nothing,
+# )
--- examples/harrypotterlogical.jl
+++ examples/harrypotterlogical.jl
@@ -19,56 +19,56 @@ Are twins once you taste them, though different at first sight.
 """
 
 # Codes:
-# 1 is netted wine
-# 2 move ahead
-# 3 move back
-# 4 kill
+# 1 is netted wine - NW
+# 2 move ahead     - MA
+# 3 move back      - MB
+# 4 poison         - PO
 
 # Seven Potions a,b,c,d,e,f,g each have 1 of 4 possible values
 # a is left of b which is left of c etc.
-logicset = logicalparse(["a, b, c, d, e, f, g  ∈  1:4"])
+logicset = logicalparse(["a, b, c, d, e, f, g  ∈  NW, MA, MB, PO"])
 
 # Two among our number hold only nettle wine, #code 1
-logicset = logicalparse(["{{i}} == 1 {{2}}"], logicset=logicset); showfeasible(logicset)
+logicset = logicalparse(["{{i}} == 'NW' {{2}}"], logicset=logicset); showfeasible(logicset)
 # Equivalent to logicalparse(["a|b|c|d|e|f|g == 1 {2}"], logicset=logicset)
 
 # One among us seven will let you move ahead, #code 2
-logicset = logicalparse(["{{i}} == 2 {{1}}"], logicset=logicset); showfeasible(logicset)
+logicset = logicalparse(["{{i}} == 'MA' {{1}}"], logicset=logicset); showfeasible(logicset)
 #logicset = logicalparse(["a|b|c|d|e|f|g == 2 {1}"], logicset=logicset)
 
 #Another will transport the drinker back instead, #code 3
-logicset = logicalparse(["{{i}} == 3 {{1}}"], logicset=logicset); showfeasible(logicset)
+logicset = logicalparse(["{{i}} == 'MB' {{1}}"], logicset=logicset); showfeasible(logicset)
 
 # Three of us are killers #code 4
-logicset = logicalparse(["{{i}} == 4 {{3}}"], logicset=logicset); showfeasible(logicset)
+logicset = logicalparse(["{{i}} == 'PO' {{3}}"], logicset=logicset); showfeasible(logicset)
 # This last specification does not reduce the feasible set
 
 # First, however slyly the poison tries to hide
 # You will always find some on nettle wine’s left side;
 # So if b is poison then a is nettle wine
-logicset = logicalparse(["{{i+1}} == 1 ==> {{i}} == 4"], logicset=logicset); showfeasible(logicset)
+logicset = logicalparse(["{{i+1}} == 'NW' ==> {{i}} == 'PO'"], logicset=logicset); showfeasible(logicset)
 # 120 Possibilities
 
 # Wine cannot be in the first bottle since poison is to the left
-logicset = logicalparse(["a != 1"], logicset=logicset); showfeasible(logicset)
+logicset = logicalparse(["a != 'NW'"], logicset=logicset); showfeasible(logicset)
 # 60 possible outcomes
 
 # Second, different are those who stand at either end,
 logicset = logicalparse(["a != g"], logicset=logicset)
 
 # But if you would move onwards, neither is your friend;
-logicset = logicalparse(["a,g != 2 "], logicset=logicset); showfeasible(logicset)
+logicset = logicalparse(["a,g != 'MA'"], logicset=logicset); showfeasible(logicset)
 # 30 possible outcomes
 
 # But if you would move onwards, neither is your friend;
 # Third, as you see clearly, all are different size,
 # Neither dwarf nor giant holds death in their insides;
 # c dwarf, f are giant
-logicset = logicalparse(["c,f != 4"], logicset=logicset); showfeasible(logicset)
+logicset = logicalparse(["c,f != 'PO'"], logicset=logicset); showfeasible(logicset)
 
 # Fourth, the second left and the second on the right
 # Are twins once you taste them, though different at first sight.
-logicset = logicalparse(["b != f === a == c"], logicset=logicset)
+logicset = logicalparse(["b == f"], logicset=logicset)
 
-search("=2", logicset) # c is a match, for the third bottle
-search("=3", logicset) # g is a match, for the last bottle
+search("='MA'", logicset) # c is a match, for the third bottle
+search("='MB'", logicset) # g is a match, for the last bottle
