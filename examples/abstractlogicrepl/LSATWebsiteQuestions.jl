db9d022366b77c9730ac4b50423518f6f2b98f43
Francis C Smart <Fsmart@gmail.com>
2019-09-16 13:55:18 -0400

    Update LSATWebsiteQuestions.jl

--- examples/abstractlogicrepl/LSATWebsiteQuestions.jl
+++ examples/abstractlogicrepl/LSATWebsiteQuestions.jl
@@ -419,7 +419,7 @@ bstractlogic> l >=4
 l >=4            Feasible Outcomes: 12   Perceived Outcomes: 1296 ✓      :6 5 2 3 4 1
 
 abstractlogic> compare refset
-active set and refset have the same number of feasible values. nfeasible(replset[:]) == nfeasible(refset[:])
+active set and refset have the same number of feasible values. nfeasible(replset) == nfeasible(refset)
 
 # Laundry has to be either immediately before or immediately after jogging.
 abstractlogic> restore
@@ -429,7 +429,7 @@ abstractlogic> l = j + 1 ||| l = j - 1
 l = j + 1 ||| l = j - 1      Feasible Outcomes: 6    Perceived Outcomes: 600 ✓       :6 5 2 4 3 1
 
 abstractlogic> compare refset
-active set has less feasible values than refset. nfeasible(replset[:]) < nfeasible(refset[:])
+active set has less feasible values than refset. nfeasible(replset) < nfeasible(refset)
 
 abstractlogic> restore
 Restoring State - Feasible Outcomes: 24      Perceived Outcomes: 11520  :1 2 3 4 5 6
@@ -449,7 +449,7 @@ abstractlogic> l < h
 l < h            Feasible Outcomes: 16   Perceived Outcomes: 2304 ✓      :6 5 2 3 4 1
 
 abstractlogic> compare refset
-replset set has more feasible values than refset. nfeasible(replset[:]) > nfeasible(refset[:])
+replset set has more feasible values than refset. nfeasible(replset) > nfeasible(refset)
 
 abstractlogic> restore
 Restoring State - Feasible Outcomes: 24      Perceived Outcomes: 11520  :1 2 3 4 5 6
@@ -459,4 +459,4 @@ abstractlogic> l < j
 l < j            Feasible Outcomes: 12   Perceived Outcomes: 2025 ✓      :4 3 6 2 1 5
 
 abstractlogic> compare refset
-replset set and refset have the same number of feasible values. nfeasible(replset[:]) == nfeasible(refset[:])
+replset set and refset have the same number of feasible values. nfeasible(replset) == nfeasible(refset)
