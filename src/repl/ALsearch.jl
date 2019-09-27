63108d6ffa1a840a5904ca0cf3dc98d5ddeb06de
Francis Smart <francis.smart@gmail.com>
2019-09-26 20:42:31 -0400

    Fixed Verbose

--- src/repl/ALsearch.jl
+++ src/repl/ALsearch.jl
@@ -1,7 +1,7 @@
 function ALsearch(userinput; verbose=true)
     try
       checker = replace(userinput[7:end], r"^[\\:\\-\\ ]+"=>"")
-      search(checker, replset, verbose = replcmdverbose & replverboseall)
+      search(checker, replset, verbose = verbose)
       # push!(logicset, replset)
   catch er
       replthrow("Warning! Search Failed: $er")
