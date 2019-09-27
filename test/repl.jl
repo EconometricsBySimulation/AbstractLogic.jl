485ee15a6300e3849e559a67f5c526c2a2305833
Francis C Smart <Fsmart@gmail.com>
2019-09-27 17:41:29 -0400

    Update repl.jl

--- test/repl.jl
+++ test/repl.jl
@@ -92,7 +92,7 @@ output = (@capture_out abstractlogic("a=b")) |> printcleaner
 @capture_out(AbstractLogic.parse_to_expr("clear")) == "Clearing Activeset\n"
 
 # Running examples, skipping 4 as it is very long
-@test length(@capture_out abstractlogic("t(1,3)")) > 10000
-@test length(@capture_out abstractlogic("t(5,10)")) > 10000
+@test length(@capture_out abstractlogic("t(1,3)")) > 1000
+@test length(@capture_out abstractlogic("t(5,10)")) > 1000
 
 @suppress abstractlogic("t(undefinedtest)"); @test replerror
